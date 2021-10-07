a3f1b110a02efe62987981495c8f0bb1bc3226b8
Fernando Lira <it.fernandolira@gmail.com>
2021-10-07 19:43:19 +0100

    Registo de Categorias e Slugify

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("./Category");
+const slugify = require("slugify");
 
 router.get("/categories",(req, res) => {
     res.send("Categorias");
@@ -8,6 +10,22 @@ router.get("/categories",(req, res) => {
 router.get("/admin/categories/new",(req, res) => {
     //res.send("Adicionar Categoria");
     res.render("admin/categories/new");
+});
+
+router.post("/categories/save", (req, res) => {
+    var title = req.body.title;
+    if(title != undefined){
+        
+        Category.create({
+            title: title,
+            slug: slugify(title)
+        }).then(() => {
+            res.redirect("/admin/categories");
+        })
+
+    }else{
+        res.redirect("/admin/categories/new");
+    }
 });
 
 module.exports = router;
