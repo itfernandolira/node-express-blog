2f361b412508848afc3cd123b12efe7b872da2e8
Fernando Lira <it.fernandolira@gmail.com>
2021-10-07 12:46:36 +0100

    Base do projeto

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const connection = require("./database/database");
+
+//database
+//CREATE database `blog` DEFAULT CHARACTER SET utf8 ;
+connection
+    .authenticate()
+    .then(() => {
+        console.log("Conexão feita com a base de dados!")
+    })
+    .catch((msgErro) => {
+        console.log(msgErro);
+    })
+
+// Instrução para o Express usar o EJS como View engine
+app.set('view engine','ejs');
+
+app.use(express.static('public'));
+
+// Body parser
+//app.use(bodyParser.urlencoded({extended: true}));
+//app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+app.get("/",function(req,res){
+        //res.send("Ok!");
+        res.render("index");
+});
+
+app.listen(4000,()=>{console.log("Servidor ativo!");});
