7e7042e7eb118fb49eca10bec491cb338c51f77e
Fernando Lira <it.fernandolira@gmail.com>
2021-10-07 21:53:20 +0100

    Acertar o timezone

--- database/database.js
+++ database/database.js
@@ -2,7 +2,8 @@ const Sequelize = require('sequelize');
 
 const connection = new Sequelize('blog','root','',{
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: "+01:00"
 });
 
 module.exports = connection;
