29bb032472cec46cda4dfadca6a2142505d8b6ad
Fernando Lira <it.fernandolira@gmail.com>
2021-10-09 06:56:54 +0100

    Editar Artigo

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -56,4 +56,20 @@ router.post("/articles/delete", (req, res) => {
     }
 });
 
+router.get("/admin/articles/edit/:id", (req, res) => {
+    var id = req.params.id;
+    Article.findByPk(id).then(article => {
+        if(article != undefined){
+            Category.findAll().then(categories => {
+                res.render("admin/articles/edit", {categories: categories, article: article})
+
+            });
+        }else{
+            res.redirect("/");
+        }
+    }).catch(err => {
+        res.redirect("/");
+    });
+});
+
 module.exports = router;
