a830b423fe9b5cc42ef8f50efdcc6bc3998b27c9
Fernando Lira <it.fernandolira@gmail.com>
2021-10-09 08:10:11 +0100

    Registar User

--- users/UsersController.js
+++ users/UsersController.js
@@ -1,9 +1,36 @@
 const express = require("express");
 const router = express.Router();
 const User = require("./User");
+const bcrypt = require('bcryptjs');
 
 router.get("/admin/users/new",(req, res) => {
     res.render("admin/users/new");
+});
+
+router.post("/users/save", (req, res) => {
+    var email = req.body.email;
+    var password = req.body.password;
+    
+    User.findOne({where:{email: email}}).then( user => {
+        if(user == undefined){
+
+            var salt = bcrypt.genSaltSync(10);
+            var hash = bcrypt.hashSync(password, salt);
+            
+            User.create({
+                email: email,
+                password: hash
+            }).then(() => {
+                res.redirect("/");
+            }).catch((err) => {
+                res.redirect("/");
+            });
+
+
+        }else{
+            res.redirect("/admin/users/new");
+        }
+    });
 });
 
 module.exports = router;
